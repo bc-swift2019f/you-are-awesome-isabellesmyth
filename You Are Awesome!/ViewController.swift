b8f878fd3d2cee89665d4385d74bda35c4ee610c
Isabelle Smyth <smythi@bc.edu>
2019-09-02 10:47:28 -0400

    Initial Commit

---
+++ You Are Awesome!/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  You Are Awesome!
+//
+//  Created by Isabelle Smyth on 9/2/19.
+//  Copyright © 2019 Isabelle Smyth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
