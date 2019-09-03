10293d347ba622a7eaf503dbcbbecd3cd0ad091d
Isabelle Smyth <smythi@bc.edu>
2019-09-03 12:36:59 -0400

    2.7 complete

--- You Are Awesome!/ViewController.swift
+++ You Are Awesome!/ViewController.swift
@@ -14,17 +14,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+    
         // Do any additional setup after loading the view.
     }
     @IBAction func showMessagePressed(_ sender: UIButton) {
-        if messageLabel.text == "You Are Awesome!" {
-            messageLabel.text = "You Are Great!"
+        let message1 = "You Are Awesome!"
+        let message2 = "You Are Great!"
+        let message3 = "You Are Amazing"
+        if messageLabel.text == message1 {
+            messageLabel.text = message2
         }
-        else if messageLabel.text == "You Are Great!" {
-            messageLabel.text = "You Are Amazing!"
+        else if messageLabel.text == message2 {
+            messageLabel.text = message3
         }
         else {
-            messageLabel.text = "You Are Awesome!"
+            messageLabel.text = message1
         }
         messageLabel.textColor = UIColor.red
 
